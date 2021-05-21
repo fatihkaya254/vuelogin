a5e23edd1e8638bcb1fb957569e3c30d6373e467
fatihkaya254 <fatihkaya254@gmail.com>
2021-05-21 11:40:21 +0300

    aa

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ import WaitingSMS from './controllers/waitingSMS'
 import cors from "cors"
 
 var corsOptions = {
-    origin: 'http://http://192.168.0.117:8000',
+    origin: 'http://http://192.168.1.54:8000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions));
