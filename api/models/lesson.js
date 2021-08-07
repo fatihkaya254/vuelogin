658e4b00416d48821ad0c2f9a1466df80b0d0994
fatihkaya254 <fatihkaya254@gmail.com>
2021-08-07 13:58:44 +0300

    unique

--- api/models/lesson.js
+++ api/models/lesson.js
@@ -1,19 +1,40 @@
 import mongoose from 'mongoose'
 
 const lessonSchema = mongoose.Schema({
-    groupName: {
-        type: String,
+    day: {
+        type: Number,
         required: true
     },
-    grade:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'grade'
+    hour: {
+        type: Number,
+        required: true
     },
-    user:{
-        type: [mongoose.Schema.Types.ObjectId],
+    branch:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'branch'
+    },
+    student:{
+        type: mongoose.Schema.Types.ObjectId,
         ref:'user'
-    }
+    },
+    teacher:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    },
+    group:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'group'
+    },
+    classroom:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'classroom'
+    },
 })
+
+lessonSchema.index({ day: 1, hour: 1, student: 1 }, { unique: true,  partialFilterExpression: { student: { $exists: true } } })
+lessonSchema.index({ day: 1, hour: 1, teacher: 1 }, { unique: true, partialFilterExpression: { teacher: { $exists: true } } })
+lessonSchema.index({ day: 1, hour: 1, group: 1 }, { unique: true, partialFilterExpression: { group: { $exists: true } } })
+lessonSchema.index({ day: 1, hour: 1, classroom: 1 }, { unique: true, partialFilterExpression: { classroom: { $exists: true } } })
 
 const Lesson = mongoose.model('lesson', lessonSchema)
 
