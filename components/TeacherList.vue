57e96e8858a85114bc71b673ca99db1bbd8f5830
fatihkaya254 <fatihkaya254@gmail.com>
2021-08-11 16:24:07 +0300

    tlist

--- components/TeacherList.vue
+++ components/TeacherList.vue
@@ -1,63 +1,162 @@
 <template lang="pug">
 .tearcherCards
-  .teacherCard(v-for="teacher in teacher()")
-    .cardHead
-      .profilePhoto
-        img(:src=" ourhost + teacher.profilePic" v-show="teacher.profilePic")
-        img(src="../assets/basic-profile.png" v-show="!teacher.profilePic")
-    .infoes
-      p {{ teacher.mainBranch }}
-      p {{ teacher.name }} {{ teacher.surname }}
-    .getLesson
-      | Özel Ders Al
+  .maincard
+    .teacherCard()
+      .cardHead
+        .profilePhoto
+          img(:src=" ourhost + photo" v-show="photo != ''")
+          img(src="../assets/basic-profile.png" v-show="photo == ''")
+      .infoes
+        p {{ name }} {{ surname }}
+      .getLesson
+        | Özel Ders Al
+    .schedule
+      .days(v-for="(n, day) in 7")
+        p {{days[day]}}
+        .row(v-for="(n, hour) in 24" @click="" v-show="teachersLessons()[day+'-'+hour] == 2")  {{hours[hour]}}
+  .teacherLine
+    div(v-for="teacher in teacher()" @click="lockTeacher(teacher._id)"  :class="[(teacher._id == id) ? 'linePhotoSelected' : 'linePhoto', 'linePhoto']")
+      img(:src=" ourhost + teacher.profilePic" v-show="teacher.profilePic")
+      img(src="../assets/basic-profile.png" v-show="photo == ''")
+
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 export default {
-  mounted() {
-    this.getTeachers();
-
+  async mounted() {
+    await this.getTeachers();
+    this.lockTeacher(0);
   },
   data() {
     return {
       name: "",
-      ourhost:  process.env.OUR_URL,
-    }
+      id: "",
+      teacherHours: "",
+      branches: [],
+      days: [
+        "Pazartesi",
+        "Salı",
+        "Çarşamba",
+        "Perşembe",
+        "Cuma",
+        "Cumartesi",
+        "Pazar"
+      ],
+      hours: [
+        "00:00",
+        "01:00",
+        "02:00",
+        "03:00",
+        "04:00",
+        "05:00",
+        "06:00",
+        "07:00",
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00",
+        "23:00"
+      ],
+      phoneLength: 11,
+      surname: "",
+      phone: "",
+      date: "",
+      photo: "",
+      adress: "",
+      email: "",
+      mainBranch: "",
+      ourhost: process.env.OUR_URL,
+      selectedGrade: ""
+    };
   },
   methods: {
     ...mapGetters("users", ["teacher", "teachersLessons"]),
-    ...mapActions("users", ["getTeachers", "addTeacherLessons", "getTeachersLessons"]),
+    ...mapActions("users", [
+      "getTeachers",
+      "addTeacherLessons",
+      "getTeachersLessons"
+    ]),
+    lockTeacher: function(who) {
+      if (who == 0) {
+        var myTeacher = this.teacher()[Object.keys(this.teacher())[who]];
+      } else {
+        var myTeacher = this.teacher()[who];
+      }
+      this.id = myTeacher._id;
+      this.name = myTeacher.name;
+      this.surname = myTeacher.surname;
+      if (myTeacher.profilePic) this.photo = myTeacher.profilePic;
+    }
   },
-  watch: {},
+  watch: {
+    id(value) {
+      if (value != "") {
+        let ta = this.teacher()[value];
+        this.name = ta.name;
+        this.surname = ta.surname;
+        this.mainBranch = ta.mainBranch;
+        this.getTeachersLessons(value);
+      }
+    }
+  }
 };
-
 </script>
 
 <style lang="sass" scoped>
 
-.tearcherCards 
-  display: flex
+.tearcherCards
   align-items: center
   justify-content: center
   min-height: 75vh
   line-height: 1.5
   font-family: "Lato"
   -webkit-font-smoothing: antialiased
-  color: white
-
+  display: flex
+  flex-direction: column
 .teacherCard
-  margin: 15px
-  height: 400px
-  width: 300px
-  border: 1px solid black
-  border-radius: 1em
+  height: 100%
+  width: 20%
   overflow: auto
+  position: absolute
+
+.schedule
+  height: 100%
+  width: 80%
+  float: right
+  display: flex
+  flex-direction: row
+
+
+.days
+    height: 100%
+    width: 100%
+    margin-left: 5px
+    text-align: center
+    padding: 10px
+
+.maincard
+  height: 400px
+  width: 60%
+  border: 1px solid black
   position: relative
+  border-radius: 1em
+
 .cardHead
   height: 120px
   width: 100%
-  background-color: #ff6262
+
 
 .profilePhoto
     height: 102px
@@ -71,6 +170,29 @@ export default {
         width: 102px
         border-radius: 50%
 
+.linePhoto
+    height: 60px
+    width: 60px
+    border-radius: 50%
+    margin: auto
+    transition: all 0.5s ease
+    & img
+        height: 60px
+        width: 60px
+        border-radius: 50%
+        transition: all 0.5s ease
+
+.linePhotoSelected
+    height:72px
+    width: 72px
+    border-radius: 50%
+    margin: auto
+    transition: all 0.5s ease
+    & img
+        height: 72px
+        width: 72px
+        border-radius: 50%
+        transition: all 0.5s ease
 .infoes
     color: black
     margin-top: 70px
@@ -80,11 +202,38 @@ export default {
     padding-top: 14px
     height: 48px
     width: 100%
-    background-color: #ff6262
     position: absolute
     bottom: 0
     text-align: center
     cursor: pointer
-    &:hover
-      background-color: #00ca4e
+
+.teacherLine
+    height: 80px
+    margin-top: 28px
+    width: 60%
+    display: flex
+
+.row
+    height: 20px
+    width: 80%
+    border-radius: 1em
+    border: 1px solid black
+    margin: auto
+    margin-top: 16px
+    font-size: 14px
+
+::-webkit-scrollbar
+    height: 10px
+    width: 2px
+    border-radius: 1em
+::-webkit-scrollbar-track
+    background: #ffffff
+    border-radius: 1em
+
+::-webkit-scrollbar-thumb
+    background: #555
+    border-radius: 1em
+
+::-webkit-scrollbar-thumb:hover
+    background: #555
 </style>
